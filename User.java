07ac5b833a7ab5fe73286d8c47834f111fa3cb93
mattgray7 <matthew.gray@mail.mcgill.ca>
2013-11-06 15:09:35 -0800

    First User class, will need to be changed to player class

--- User.java
+++ User.java
@@ -1,8 +1,76 @@
+
 public class User {
-	public void readCSV() {
-		
+
+	private int direction;
+	private int prevDirection;
+	private int positionX;
+	private int positionY;
+	private int prevPositionX;
+	private int prevPositionY;
+	private int roundsWon;
+	private String username;
+	
+	public int getDirection(){
+		return direction;
 	}
-	public void updateCSV() {
-		
+	
+	public int getPrevDirection(){
+		return prevDirection;
+	}
+	
+	public int getPositionX(){
+		return positionX;
+	}
+	
+	public int getPositionY(){
+		return positionY;
+	}
+	
+	public int getPrevPositionX(){
+		return prevPositionX;
+	}
+	
+	public int getPrevPositionY(){
+		return prevPositionY;
+	}
+	
+	public int getRoundsWon(){
+		return roundsWon;
+	}
+	
+	public String getUsername(){
+		return username;
+	}
+	
+	public void setDirection(int dir){
+		direction = dir;
+	}
+	
+	public void setPrevDirection(int dir){
+		prevDirection = dir;
+	}
+	
+	public void setPositionX(int x){
+		positionX = x;
+	}
+	
+	public void setPositionY(int y){
+		positionY = y;
+	}
+	
+	public void setPrevPositionX(int x){
+		prevPositionX = x;
+	}
+	
+	public void setPrevPositionY(int y){
+		prevPositionY = y;
+	}
+	
+	public void setRoundsWon(int rw){
+		roundsWon = rw;
+	}
+	
+	public void setUsername(String usr){
+		username = usr;
 	}
 }
